5512c11bb237588d795fe55bda943139d3b66e07
Houssem GHIAT <ghiat.houssem@gmail.com>
2018-06-11 14:39:57 +0100

    try new stuff

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,15 +1,9 @@
 extern crate geomprim2d;
 
 use geomprim2d::Point;
+use geomprim2d::Triangle;
 
 fn main() {
-  let p1: Point<i32> = Point::origin();
-  let mut p2: Point<f64> = Point::from_coords(4f64, 3f64);
-  println!("{:?}", p1);
-  println!("{:?}", p2);
-  p2.translate(0f64, 5f64);
-  let p3 = Point::from_point(&p2);
-  let _p4 = p3;
-  println!("translated by 0,5 : {:?}", p2);
-  println!("{:?}", p3);
+  let t1 = Triangle::new(Point::new(1, 1), Point::new(1, 3), Point::new(3, 4));
+  println!("{:?}", t1);
 }
